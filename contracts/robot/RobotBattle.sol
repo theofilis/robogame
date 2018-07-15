72377620af95633f8bb75cc24ccb424980360dec
George Theofilis <theofilis.g@gmail.com>
2018-07-15 12:54:28 +0300

    Add battle contract

---
+++ contracts/robot/RobotBattle.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.23;
+
+import "./RobotHelper.sol";
+
+contract RobotBattle is RobotHelper {
+    uint randNonce = 0;
+    uint attackVictoryProbability = 70;
+
+    function randMod(uint _modulus) internal returns(uint) {
+        randNonce = randNonce.add(1);
+        return uint(keccak256(block.number, msg.sender, randNonce)) % _modulus;
+    }
+
+    function attack(uint _robotId, uint _targetId) external 
+        onlyOwnerOf(_robotId) 
+        isReady(_robotId)
+    {
+        Robot storage myRobot = robots[_robotId];
+        Robot storage enemyRobot = robots[_targetId];
+
+        uint rand = randMod(100);
+        if (rand <= attackVictoryProbability) {
+            myRobot.winCount = myRobot.winCount.add(1); 
+            enemyRobot.lossCount = enemyRobot.lossCount.add(1);
+            myRobot.level = myRobot.level.add(1); 
+        } else {
+            enemyRobot.winCount = enemyRobot.winCount.add(1);
+            myRobot.lossCount = myRobot.lossCount.add(1);
+            enemyRobot.level = enemyRobot.level.add(1);
+        }
+
+        triggerCooldown(myRobot);
+    }
+}
--- contracts/robot/RobotHelper.sol
+++ contracts/robot/RobotHelper.sol
@@ -15,9 +15,18 @@ contract RobotHelper is RobotFactory {
         _;
     }
 
+    modifier isReady(uint _robotId) {
+        require(robots[_robotId].readyTime <= block.number);
+        _;
+    }
+
+    function triggerCooldown (Robot storage _robot) internal {
+        _robot.readyTime = uint32(block.number + cooldownTime);
+    }
+
     function levelUp(uint _robotId) external payable {
         require(msg.value == levelUpFee);
-        robots[_robotId].level++;
+        robots[_robotId].level = robots[_robotId].level.add(1);
     }
 
     function setLevelUpFee(uint _fee) external onlyOwner {
@@ -27,15 +36,19 @@ contract RobotHelper is RobotFactory {
     function changeName(uint _robotId, string _newName) external 
         onlyOwnerOf(_robotId)
         aboveLevel(_robotId, 2)
+        isReady(_robotId)
     {        
         robots[_robotId].name = _newName;
+        triggerCooldown(robots[_robotId]);
     }
 
     function changeDna(uint _robotId, uint _newDna) external 
         onlyOwnerOf(_robotId) 
-        aboveLevel(_robotId, 20) 
+        aboveLevel(_robotId, 20)
+        isReady(_robotId)
     {
         robots[_robotId].dna = _newDna;
+        triggerCooldown(robots[_robotId]);
     }
 
     function withdraw() external onlyOwner { 
